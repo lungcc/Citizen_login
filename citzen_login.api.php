e68d3d1498e89dd3ed4d29967502d475439ceed2
guilherme lopes <guiajlopes@gmail.com>
2014-09-22 20:24:33 -0300

    Add citzen_login.api.php

---
+++ citzen_login.api.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Implements hook_citzen_login_scope_alter().
+ *
+ * @param array $scope
+ *   API Scope in Citzen login.
+ */
+function hook_citzen_login_scope_alter(&$scope) {
+  // Your code here.
+}
